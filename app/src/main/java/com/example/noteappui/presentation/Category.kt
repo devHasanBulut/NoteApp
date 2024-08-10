1d1284d8d5a9220dd9e041dbb88b82b7305efa12
Enes Duhan Bulut <dev.duhanbulut@gmail.com>
2024-08-10 09:30:46 +0300

    gm

--- app/src/main/java/com/example/noteappui/presentation/Category.kt
+++ app/src/main/java/com/example/noteappui/presentation/Category.kt
@@ -20,7 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-
+//
 @Composable
 fun Category(
     categoryViewEntity: CategoryViewEntity,
